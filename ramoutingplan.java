d386df076ce945e16ed6ffa0dd6d49aec5f8f29d
chidimillasupraja <chidimillasupraja@gmail.com>
2025-07-24 13:29:37 +0530

    Add files via upload

---
+++ ramoutingplan.java
@@ -0,0 +1,12 @@
+class RamoutingPlan{
+public static void main(String[] args){
+int temp=70;
+if (temp>=45){
+  System.out.println("ram should staty home");
+}else if (temp>=30 && temp<=44){
+  System.out.println("ram should carry water");
+}else{
+System.out.println("ram can go out normally");
+}
+}
+}
---
+++ shyaminsurance.java
@@ -0,0 +1,19 @@
+class ShyamInsurance{
+ public static void main(String[] args){
+int age=70;
+System.out.println("Shyam applied for health insurance.");
+if(age>60){
+System.out.println(" premium is ₹5000.");
+}else if(age>=40 && age<=59){
+System.out.println("premium is ₹3000.");
+}else if(age>=20 && age<=39){
+System.out.println("premium is ₹1500.");
+}
+else{
+System.out.println("premium is ₹1000.");
+}
+}
+}
+
+
+
